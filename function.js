ea4f13a5b62e161a1cdef2dab7c5c03e308ce5f2
Reka K <reka.k@wavemyth.com>
2023-06-15 16:31:23 +0300

    js

---
+++ function.js
@@ -0,0 +1,14 @@
+var question = prompt("How are you?");
+
+console.log('teest');
+
+if (question == 'good') {
+  console.log('test')
+  $('.hero-description').css({'display':'none'})
+  console.log('heheh');
+} else {
+  alert("Hello! I am an alert box!!");
+}
+
+
+
